4ae0f70b89cf146d2f0eb156c35224c99785a571
ahmet erbay <erbayusa@gmail.com>
2022-10-29 08:15:53 -0700

    ikinci taski bitirdim

--- src/Gun01/merhaba.java
+++ src/Gun01/merhaba.java
@@ -2,6 +2,9 @@ package Gun01;
 
 public class merhaba {
     public static void main(String[] args) {
-
+        System.out.println("merhaba");
+        for (int i=0; i<5 ; i++){
+            System.out.println("merhaba"+i);
+        }
     }
 }
